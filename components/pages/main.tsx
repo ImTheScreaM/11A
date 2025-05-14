f7206e94f83aab0daae9c278cb0cf9ecd354ef80
Miracle <godbeerus@mail.ru>
2025-05-14 17:40:26 +0300

    v1.4 add gsap

--- components/pages/main.tsx
+++ components/pages/main.tsx
@@ -1,21 +1,33 @@
 'use client'
 
-
-
 import '@/components/assets/css/main.css'
+import { LeftTriger, RightTriger } from '@/core/hooks/scrollTriger'
 import { usePersoneStore } from '@/core/store/userCreate'
 import { IUser } from '@/core/types/types'
-
-import Link from 'next/link'
 import { ImgSlider } from '../ui/swiper/swiper'
+
+import gsap from 'gsap'
+import Link from 'next/link'
+import { useEffect, useRef } from 'react'
+
 
 export const Main = () => {
 	const name = usePersoneStore(state => state.data)
+	const box = useRef(null)
 
+	useEffect(() => {
+		if (typeof window != 'undefined') {
+			gsap.fromTo(
+				box.current,
+				{ opacity: 0, x: -150 },
+				{ opacity: 1, x: 0, duration: 1, ease: 'power2.out' }
+			)
+		}
+	}, [])
 
 	return (
 		<main className='container_main'>
-			<div className='entry'>
+			<div className='entry' ref={box}>
 				<h1>Выпускники 2025 года (ОГБОУ "Вейделевская СОШ")</h1>
 				<h3>
 					Добро пожаловать на сайт наших выпускников! Этот ресурс создан, чтобы
@@ -25,60 +37,66 @@ export const Main = () => {
 				</h3>
 			</div>
 
-			<div className='container_about_site'>
-				<div className='about_site'>
-					<span className='about_site_title' style={{ fontSize: '45px' }}>
-						О чем этот сайт?
-					</span>
-					<div className='about_site_information'>
-						<div id='about_pict_pages'>
-							<div className='about_site_pict'>
-								<span style={{ fontSize: '35px' }}>Фотки</span>
-								<div style={{ margin: '2px' }}>
-									<h3>Фотки нашего класса (вместе и отдельно)</h3>
+			<RightTriger>
+				<div className='container_about_site'>
+					<div className='about_site'>
+						<span className='about_site_title' style={{ fontSize: '45px' }}>
+							О чем этот сайт?
+						</span>
+						<div className='about_site_information'>
+							<div id='about_pict_pages'>
+								<div className='about_site_pict'>
+									<span style={{ fontSize: '35px' }}>Фотки</span>
+									<div style={{ margin: '2px' }}>
+										<h3>Фотки нашего класса (вместе и отдельно)</h3>
+									</div>
 								</div>
 							</div>
-						</div>
-						<div id='about_pict_pages'>
-							<div className='about_site_pages'>
-								<span style={{ fontSize: '35px' }}>Странички</span>
-								<div style={{ margin: '2px' }}>
-									<h3>Личные странички где будет информация о каждом</h3>
+							<div id='about_pict_pages'>
+								<div className='about_site_pages'>
+									<span style={{ fontSize: '35px' }}>Странички</span>
+									<div style={{ margin: '2px' }}>
+										<h3>Личные странички где будет информация о каждом</h3>
+									</div>
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-			</div>
+			</RightTriger>
 
-			<div className='container_album--pictures'>
-				<div>
-					<span style={{ fontSize: '35px' }}>Фотогалерея</span>
-					<div className='img_container'>
-						<ImgSlider/>
-				</div>
-				</div>
-			</div>
-
-			<div className='container_class-pages'>
-				<div className='class-pages'>
-					<span style={{ fontSize: '35px' }}>Странички</span>
-					<div className='container_users'>
-						{name.map((item: IUser) => (
-							<Link
-								href={`/users/${item.id}`}
-								key={item.id}
-								className='item_users'
-							>
-								<div className='item_img-users'>
-									<img src={item.img.src} id={item.idClass} />
-								</div>
-								<span className='name_users'>{item.name}</span>
-							</Link>
-						))}
+			<LeftTriger>
+				<div className='container_album--pictures'>
+					<div>
+						<span style={{ fontSize: '35px' }}>Фотогалерея</span>
+						<div className='img_container'>
+							<ImgSlider />
+						</div>
 					</div>
 				</div>
-			</div>
+			</LeftTriger>
+
+			<RightTriger>
+				<div className='container_class-pages'>
+					<div className='class-pages'>
+						<span style={{ fontSize: '35px' }}>Странички</span>
+						<div className='container_users'>
+							{name.map((item: IUser) => (
+								<Link
+									href={`/users/${item.id}`}
+									key={item.id}
+									className='item_users'
+								>
+									<div className='item_img-users'>
+										<img src={item.img.src} id={item.idClass} />
+									</div>
+									<span className='name_users'>{item.name}</span>
+								</Link>
+							))}
+						</div>
+					</div>
+				</div>
+			</RightTriger>
 		</main>
 	)
 }
---
+++ core/hooks/scrollTriger.tsx
@@ -0,0 +1,58 @@
+'use client'
+
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import { useEffect, useRef } from 'react'
+gsap.registerPlugin(ScrollTrigger)
+
+export const LeftTriger = ({ children }) => {
+	const scrollRef = useRef(null)
+
+	useEffect(() => {
+		if (typeof window != 'undefined') {
+			gsap.fromTo(
+				scrollRef.current,
+				{ opacity: 0, x: -450 },
+				{
+					opacity: 1,
+					x: 0,
+					duration: 1,
+					ease: 'power2.out',
+					scrollTrigger: {
+						trigger: scrollRef.current,
+						start: 'top 80%',
+						toggleActions: 'play none none none',
+					},
+				}
+			)
+		}
+	}, [])
+
+	return <section ref={scrollRef}>{children}</section>
+}
+
+export const RightTriger = ({ children }) => {
+	const scrollRef = useRef(null)
+
+	useEffect(() => {
+		if (typeof window != 'undefined') {
+			gsap.fromTo(
+				scrollRef.current,
+				{ opacity: 0, x: 450 },
+				{
+					opacity: 1,
+					x: 0,
+					duration: 1,
+					ease: 'power2.out',
+					scrollTrigger: {
+						trigger: scrollRef.current,
+						start: 'top 80%',
+						toggleActions: 'play none none none',
+					},
+				}
+			)
+		}
+	}, [])
+
+	return <section ref={scrollRef}>{children}</section>
+}
